659f39dea67c968388df9e6a85634afb97268432
Programmer Ammo <100178397+EternalBlue3@users.noreply.github.com>
2023-05-23 15:50:39 -0400

    Delete Recognizer.py

--- Recognizer.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-def load_model():
-    recognizer = tf.keras.models.load_model("Digits_Recognizer")
-    probability_model = tf.keras.Sequential([recognizer, tf.keras.layers.Softmax()])
-    return probability_model
