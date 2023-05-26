b6dea17b309ea6e6789f52085348990158c35fe3
Programmer Ammo <100178397+EternalBlue3@users.noreply.github.com>
2023-05-26 15:45:20 -0400

    Update Recognizer.py

--- Recognizer.py
+++ Recognizer.py
@@ -1,2 +1,2 @@
 import tensorflow as tf
-probability_model = tf.keras.Sequential([tf.keras.models.load_model("MNIST-AI.h5"), tf.keras.layers.Softmax()])
+probability_model = tf.keras.Sequential([tf.keras.models.load_model("MNIST.h5"), tf.keras.layers.Softmax()])
